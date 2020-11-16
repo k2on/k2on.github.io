53762ad429f8860b4e057456b71db2002769c1d7
Max Koon <theakman96@gmail.com>
2020-11-16 17:23:22 -0500

    added more messages

--- script.sh
+++ script.sh
@@ -1,9 +1,22 @@
-echo "-- Setting up .zshrc --"
+# Welcome myself!
+name="$(tr '[:lower:]' '[:upper:]' <<< ${USER:0:1})${USER:1}"
+echo ""
+echo "🚀 Welcome $name\!"
+echo ""
 
+# ZSH setup
+echo "Downloading your ZSH profile"
+
+# Set the download URL
 DOWNLOAD_URL="https://koon.dev/zshrc.sh"
 
+# Download the profile to the .zshrc file
 curl -o "$HOME/.zshrc" -L --progress-bar $DOWNLOAD_URL
 
-echo "-- installed .zshrc"
+echo "ZSH profile set up!"
+
+# Success
+echo ""
+echo "✨ All done!"
 
 exit 0
