46db0839369887194cea3dd380fb6644c16704c0
Max Koon <theakman96@gmail.com>
2020-11-16 21:57:24 -0500

    added menu

--- script.sh
+++ script.sh
@@ -1,22 +1,104 @@
+#!/usr/bin/env bash
+
+# Menu creation
+
+function select_option {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        { read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi; }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - $#))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for opt; do
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
+
+# Install ZSH function
+
+install_zsh ()
+{
+
+    # ZSH setup
+    echo "Downloading your ZSH profile"
+
+    # Set the download URL
+    DOWNLOAD_URL="https://koon.dev/zshrc.sh"
+
+    # Download the profile to the .zshrc file
+    curl -o "$HOME/.zshrc" -L --progress-bar $DOWNLOAD_URL
+
+    echo "ZSH profile set up!"
+
+    # Success
+    echo ""
+    echo "✨ All done!"
+
+    exit 0
+}
+
+options=("setup" "zsh" "bot")
+
 # Welcome myself!
 name="$(tr '[:lower:]' '[:upper:]' <<< ${USER:0:1})${USER:1}"
 echo ""
 echo "🚀 Welcome $name!"
 echo ""
-
-# ZSH setup
-echo "Downloading your ZSH profile"
-
-# Set the download URL
-DOWNLOAD_URL="https://koon.dev/zshrc.sh"
-
-# Download the profile to the .zshrc file
-curl -o "$HOME/.zshrc" -L --progress-bar $DOWNLOAD_URL
-
-echo "ZSH profile set up!"
-
-# Success
+echo "What would you like to do?"
 echo ""
-echo "✨ All done!"
 
-exit 0
+select_option "${options[@]}"
+choice=$?
+
+echo "Choosen index = $choice"
+echo "        value = ${options[$choice]}"
+
+
+
+
