c0147ccecd1c38427e1dc62784be6017e909f21c
Max Koon <theakman96@gmail.com>
2020-11-27 21:02:52 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# kind
+curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/bin/kind
+
+# pfetch
+curl -sLo ./pfetch https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch
+mv ./pfetch /usr/bin/pfetch
