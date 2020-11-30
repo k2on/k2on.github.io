4261c1f9798d5d1c0b56f0f63c8b4a69f5de2220
Max Koon <theakman96@gmail.com>
2020-11-30 11:11:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
 # kind
 curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/bin/kind
+sudo mv ./kind /usr/bin/kind
 
 # pfetch
 curl -sLo ./pfetch https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch
-mv ./pfetch /usr/bin/pfetch
+sudo mv ./pfetch /usr/bin/pfetch
